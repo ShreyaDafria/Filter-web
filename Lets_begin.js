5649d4ab944bd9fc28088b4f6c4e107d73cf47bc
ShreyaDafria <73946448+ShreyaDafria@users.noreply.github.com>
2021-03-27 20:21:01 +0530

    Add files via upload

---
+++ Lets_begin.js
@@ -0,0 +1,3 @@
+function Next_Screen() {
+    window.location= "index.html"
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function preload () {
+}
+
+function draw () {
+}
+
+    function setup() {
+        canvas = createCanvas(300, 300);
+        canvas.center();
+      }
+
+
+function take_snapshot() {
+    save('My Filter.png');
+}
